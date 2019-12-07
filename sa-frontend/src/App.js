886af60f6e653178e62ac29f0ddfe7720892af8f
root <root@ip-172-31-29-148.us-west-2.compute.internal>
2019-12-07 16:16:28 +0000

    updated

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://54.190.198.83:31372/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
