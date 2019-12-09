49eec7ecb20afd7768e9bd0771fd36108fa1d3f3
Abhishek Chowdhary <andromobs5@gmail.com>
2019-12-09 21:32:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-apt-get install git maven npm python-pip -y 
-pip install awscli
+apt-get install git maven npm awscli -y 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
